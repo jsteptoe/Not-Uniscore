d7e60db291c2002343391785e3926cf76a13d047
Jake Steptoe <s5105535@bournemouth.ac.uk>
2018-12-06 16:14:30 +0000

    Uni

---
+++ Not Uniscore/Uni.swift
@@ -0,0 +1,16 @@
+import Foundation
+import MapKit
+
+class Uni: NSObject, MKAnnotation {
+    
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    let region: CLCircularRegion
+    
+    init(coordinate: CLLocationCoordinate2D, title: String) {
+        self.coordinate = coordinate
+        self.title = title
+        region = CLCircularRegion(center: coordinate, radius: 200, identifier: title)
+    }
+    
+}

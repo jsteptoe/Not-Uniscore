a6cf7c67a5a9a139621946b491da6e083b54beac
Jake Steptoe <s5105535@bournemouth.ac.uk>
2018-12-06 16:29:25 +0000

    added shit lol

---
+++ Not Uniscore/AppContainerViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class AppContainerViewController: UIViewController {
+    
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        AppManager.shared.appContainer = self
+        AppManager.shared.showApp()
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
+
+
---
+++ Not Uniscore/StudentViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+import Firebase
+import FirebaseCore
+
+
+class StudentViewController: UIViewController {
+    
+    let database = ["One", "Two", "Three"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        FirebaseApp.configure()
+        
+        let db = Firestore.firestore()
+        
+    }
+}
+

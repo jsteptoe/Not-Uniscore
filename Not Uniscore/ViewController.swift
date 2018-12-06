8de816b7bf32cd3d6a3ca2976979f20de9341711
Jake Steptoe <s5105535@bournemouth.ac.uk>
2018-12-06 16:11:00 +0000

    Initial Commit

---
+++ Not Uniscore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Not Uniscore
+//
+//  Created by Jake Steptoe on 06/12/2018.
+//  Copyright © 2018 Jake Steptoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

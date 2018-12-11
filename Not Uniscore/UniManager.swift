3392479dcba763a498aa44e1e61f621fca703f5f
Hannah St. George <s5102531@bournemouth.ac.uk>
2018-12-11 16:38:47 +0000

    added comments to uniManager.swift

--- Not Uniscore/UniManager.swift
+++ Not Uniscore/UniManager.swift
@@ -9,6 +9,8 @@ class UniManager {
     
     init() {
         
+        //the coordinate (lat and long) and uni as the title- if we expanded to more unis we could change title to each one and add images
+        
         let uniCoordinate = CLLocationCoordinate2D(latitude: 50.7424473, longitude: -1.8982909)
         let uniLocation = Uni(coordinate: uniCoordinate, title: "Uni")
         uni.append(uniLocation)

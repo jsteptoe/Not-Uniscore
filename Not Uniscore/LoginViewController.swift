1b9ed7e2399b8cb9ac7f4996ab15db19b48bc4ef
Jake Steptoe <s5105535@bournemouth.ac.uk>
2018-12-11 15:49:07 +0000

    Segue from login to map

--- Not Uniscore/LoginViewController.swift
+++ Not Uniscore/LoginViewController.swift
@@ -19,7 +19,7 @@ class LoginViewController: UIViewController {
 override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if segue.identifier == "showSecondVC" {
         let MapViewController: MapViewController = segue.destination as! MapViewController
-         destination.uni = (sender as! Uni)
+       
         
 }
 }

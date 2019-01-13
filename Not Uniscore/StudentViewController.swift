c70c4dc7af78a3dea63ebee5c75ae149b4578970
Jake Steptoe <s5105535@bournemouth.ac.uk>
2019-01-13 15:21:21 +0000

    Tried to set up firestore, ran into errors

--- Not Uniscore/StudentViewController.swift
+++ Not Uniscore/StudentViewController.swift
@@ -3,58 +3,53 @@ import Firebase
 
 class StudentViewController: UIViewController {
     
-    class StudentDatabase: UITableView{
+    class StudentDatabase: UITableViewCell{
         
     
     let database = ["One", "Two", "Three"]
+        
+
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
         //Auth.auth().currentUser!.uid
-        gotToUni()
+       
     }
     
-    in cell for row at
-    db = firestore.firestore()
-    loadData()
+
+  // func studentDatabase() {
+         let userRef = Firestore.firestore().collection("Students").document("Fred Sanders")
     
-    self.tableView = reloadData
+   // in cell for row at
+    //    db = firestore.firestore(){
+  //  loadData()
     
+  //  self.tableView = reloadData
+    }
+
     //custom cell
     //title
 
-    
-    
-    func gotToUni() {
-        
-        let userRef = Firestore.firestore().collection("Users").document("9805HxIKStYkzxgZIQXMvDJxv6H2")
-        userRef.getDocument { document, error in
-            let lastUpdated = document!.data()!["date"] as! Timestamp
-            
-            if self.hasVisitedToday(date: lastUpdated.dateValue()) {
-                print("has visited already")
-                print()
-            } else {
-                self.newVisit()
-            }
-            
-        }
 
-    }
+    
+   /// func gotToUni() {
+        
+     //   let userRef = Firestore.firestore().collection("Users").document("9805HxIKStYkzxgZIQXMvDJxv6H2")
+      
+
     
     
-    func newVisit() {
-        let timestamp = Timestamp(date: Date())
+    
+    
         
         
-    }
+
     
     
-    func hasVisitedToday(date: Date) -> Bool {
-        let cal = Calendar.current
-        return cal.isDateInToday(date)
-    }
+
+
     
-}
+
 
